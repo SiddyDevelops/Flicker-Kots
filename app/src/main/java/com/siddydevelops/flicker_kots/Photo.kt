4df61d118cd75c6a4d58635e67668226e0666db0
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-03 10:17:47 +0530

    Added Parcelable & Serializable

--- app/src/main/java/com/siddydevelops/flicker_kots/Photo.kt
+++ app/src/main/java/com/siddydevelops/flicker_kots/Photo.kt
@@ -1,42 +1,45 @@
 package com.siddydevelops.flicker_kots
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.io.IOException
 import java.io.ObjectStreamException
 import java.io.Serializable
 
+@Parcelize
 class Photo(var title:String, var author:String, var authorId:String, var link:String,
-            var tag:String, var image:String) : Serializable {
+            var tag:String, var image:String) : Parcelable {
 
-    companion object {
-        private const val serialVersionUID = 1L
-    }
+//    companion object {
+//        private const val serialVersionUID = 1L
+//    }
 
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tag='$tag', image='$image')"
     }
 
-    @Throws(IOException::class)
-    private fun writeObject(out: java.io.ObjectOutputStream) {
-        out.writeUTF(title)
-        out.writeUTF(author)
-        out.writeUTF(authorId)
-        out.writeUTF(link)
-        out.writeUTF(tag)
-        out.writeUTF(image)
-    }
-
-    @Throws(IOException::class, ClassNotFoundException::class)
-    private fun readObject(inStream: java.io.ObjectInputStream) {
-        title = inStream.readUTF()
-        author = inStream.readUTF()
-        authorId = inStream.readUTF()
-        link = inStream.readUTF()
-        tag = inStream.readUTF()
-        image = inStream.readUTF()
-    }
-
-    @Throws(ObjectStreamException::class)
-    private fun readObjectNoData() {
-
-    }
+//    @Throws(IOException::class)
+//    private fun writeObject(out: java.io.ObjectOutputStream) {
+//        out.writeUTF(title)
+//        out.writeUTF(author)
+//        out.writeUTF(authorId)
+//        out.writeUTF(link)
+//        out.writeUTF(tag)
+//        out.writeUTF(image)
+//    }
+//
+//    @Throws(IOException::class, ClassNotFoundException::class)
+//    private fun readObject(inStream: java.io.ObjectInputStream) {
+//        title = inStream.readUTF()
+//        author = inStream.readUTF()
+//        authorId = inStream.readUTF()
+//        link = inStream.readUTF()
+//        tag = inStream.readUTF()
+//        image = inStream.readUTF()
+//    }
+//
+//    @Throws(ObjectStreamException::class)
+//    private fun readObjectNoData() {
+//
+//    }
 }
--- app/src/main/java/com/siddydevelops/flicker_kots/PhotoDetailsActivity.kt
+++ app/src/main/java/com/siddydevelops/flicker_kots/PhotoDetailsActivity.kt
@@ -1,13 +1,10 @@
 package com.siddydevelops.flicker_kots
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.siddydevelops.flicker_kots.databinding.ActivityPhotoDetailsBinding
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.content_photo_details.*
 
 class PhotoDetailsActivity : BaseActivity() {
 
@@ -20,6 +17,19 @@ class PhotoDetailsActivity : BaseActivity() {
         binding = ActivityPhotoDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         activateToolbar(true)
+
+        //val photo = intent.getSerializableExtra(PHOTO_TRANSFER) as Photo
+        val photo = intent.extras?.getParcelable<Photo>(PHOTO_TRANSFER) as Photo
+
+        photo_title.text = photo.title
+        photo_tags.text = photo.tag
+        photo_author.text = photo.author
+        Picasso.get().load(photo.link)
+            .error(R.drawable.baseline_image_black_48dp)
+            .placeholder(R.drawable.baseline_image_black_48dp)
+            .into(photo_image)
+
+
 
     }
 }
