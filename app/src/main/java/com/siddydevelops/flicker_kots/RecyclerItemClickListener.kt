a7d976e9e27d5ae91987d1e0150c6e366f60f80c
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-03 08:59:26 +0530

    Added GestureDectector

--- app/src/main/java/com/siddydevelops/flicker_kots/RecyclerItemClickListener.kt
+++ app/src/main/java/com/siddydevelops/flicker_kots/RecyclerItemClickListener.kt
@@ -1,8 +1,10 @@
 package com.siddydevelops.flicker_kots
 
 import android.content.Context
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
+import androidx.core.view.GestureDetectorCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, private val listener: OnRecyclerClickListener)
@@ -13,9 +15,23 @@ class RecyclerItemClickListener(context: Context, recyclerView: RecyclerView, pr
         fun onItemLongClick(view: View, position: Int)
     }
 
+    private val gestureDectector = GestureDetectorCompat(context, object : GestureDetector.SimpleOnGestureListener(){
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            val childView = recyclerView.findChildViewUnder(e.x,e.y)
+            listener.onItemClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            val childView = recyclerView.findChildViewUnder(e.x,e.y)
+            listener.onItemLongClick(childView!!, recyclerView.getChildAdapterPosition(childView))
+        }
+    })
+
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
         //return super.onInterceptTouchEvent(rv, e)
-        return true
+        val result = gestureDectector.onTouchEvent(e)
+        return result
     }
 
 }
