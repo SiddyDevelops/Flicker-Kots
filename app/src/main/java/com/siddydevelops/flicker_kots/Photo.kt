f814ff0e8bbb1fc8e5740d0fbc4ba42029b0ab1f
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-03-03 09:42:12 +0530

    Serializable Added

--- app/src/main/java/com/siddydevelops/flicker_kots/Photo.kt
+++ app/src/main/java/com/siddydevelops/flicker_kots/Photo.kt
@@ -1,6 +1,9 @@
 package com.siddydevelops.flicker_kots
 
-class Photo(val title:String, val author:String, val authorId:String, val link:String, val tag:String, val image:String) {
+import java.io.Serializable
+
+class Photo(val title:String, val author:String, val authorId:String, val link:String,
+            val tag:String, val image:String) : Serializable {
     override fun toString(): String {
         return "Photo(title='$title', author='$author', authorId='$authorId', link='$link', tag='$tag', image='$image')"
     }
